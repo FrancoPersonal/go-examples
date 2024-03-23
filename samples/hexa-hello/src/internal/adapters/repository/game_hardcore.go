220c9525d47d7e74d031743a12087a828b2ba0f0
FrancoPersonal <jafr6@hotmail.com>
2024-03-23 06:53:03 -0600

    update hexa example

--- samples/hexa-hello/src/internal/adapters/repository/game_hardcore.go
+++ samples/hexa-hello/src/internal/adapters/repository/game_hardcore.go
@@ -1,10 +1,37 @@
 package repository
 
-import "github.com/FrancoPersonal/samples/hexa-hello/src/internal/app/ports"
+import (
+	"errors"
+
+	"github.com/FrancoPersonal/samples/hexa-hello/src/internal/app/domain"
+	"github.com/FrancoPersonal/samples/hexa-hello/src/internal/app/ports"
+)
+
+var (
+	games map[string]domain.Game
+)
 
 type GameHardCore struct {
 }
 
 func NewGameHardCore() (ports.GameRepository, error) {
+	games = make(map[string]domain.Game)
 	return GameHardCore{}, nil
+}
+
+func (repo GameHardCore) CreateGame(name string) (*domain.Game, error) {
+	if _, ok := games[name]; ok {
+		return nil, errors.New("already Exist")
+	}
+	return &domain.Game{
+		Name: name,
+	}, nil
+}
+
+func (repo GameHardCore) GetGame(name string) (*domain.Game, error) {
+	if value, ok := games[name]; !ok {
+		return nil, errors.New("not found")
+	} else {
+		return &value, nil
+	}
 }
--- samples/hexa-hello/src/internal/app/ports/repository.go
+++ samples/hexa-hello/src/internal/app/ports/repository.go
@@ -1,7 +1,10 @@
 package ports
 
-import "github.com/FrancoPersonal/samples/hexa-hello/src/internal/app/domain"
+import (
+	"github.com/FrancoPersonal/samples/hexa-hello/src/internal/app/domain"
+)
 
 type GameRepository interface {
-	CreateGame(name string) (domain.Game, error)
+	CreateGame(name string) (*domain.Game, error)
+	GetGame(name string) (*domain.Game, error)
 }
